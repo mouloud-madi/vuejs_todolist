2b9fd0d62ad3fab3aca72e6c5c864c78f19b5868
mouloudmadi <mouloutbk@gmail.com>
2020-04-28 12:04:18 +0100

    instal bootsrap vue and create the first component Navbar

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-container>
+        <b-navbar-brand href="#">Vuejs TodoList</b-navbar-brand>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
