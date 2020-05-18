f1bad675f65be0f05f072d9359e0a36b18456e4c
mouloudmadi <mouloutbk@gmail.com>
2020-05-18 11:42:18 +0100

    Corrected some mistakes and add the time in todolist

--- src/components/todos/Create.vue
+++ src/components/todos/Create.vue
@@ -4,10 +4,11 @@
       <div class="row justify-content-center" style="margin-top:100px">
         <div class="col-md-8">
           <form class="input-group" v-on:submit.prevent="AddTodo">
-            <input type="text" class="form-control" placeholder="Enter new task" v-model="name" />
+            <input type="text" class="form-control" placeholder="Enter new task" v-model.trim="name" />
             <b-form-datepicker v-model="date"></b-form-datepicker>
+            <b-form-timepicker v-model="time" ></b-form-timepicker>
             <div class="input-group-append">
-              <button class="btn btn-info" :disabled="!name || !date">+ Add</button>
+              <button class="btn btn-info" :disabled="!name"><b-icon icon="plus"></b-icon> Add</button>
             </div>
           </form>
            <List v-bind:tasks="tasks"/>
@@ -19,6 +20,7 @@
 
 <script>
 import List from '../../components/todos/List'
+import moment from 'moment'
 export default {
   name: "Create",
   components :{
@@ -27,17 +29,22 @@ export default {
   data() {
     return {
       tasks: [],
-      name: "mouloud",
-      date: ""
+      name: "",
+      date: "",
+      time: ""
     };
   },
   methods: {
     AddTodo() {
       this.tasks.unshift({
         name: this.name,
-        date: this.date,
+        date: this.date ? moment(this.date).format("D/M/YYYY") : moment().format("D/M/YYYY"),
+        time: this.time ? this.time : moment().format("HH:mm:ss"),
         status: false,
-      });
+      }); 
+      this.name=""
+      this.date=""
+      this.time=""
     }
   }
 };
--- src/components/todos/List.vue
+++ src/components/todos/List.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <ul class="list-group mt-5">
-      <li class="list-group-item d-flex justify-content-between align-items-center" v-for="(task,index) in tasks" :key="index">
-        <div class="col-5"> 
-        <b-form-checkbox  @change="task.status = !task.status"> 
-        {{task.status ? 'Done' : 'Todo'}} 
-        </b-form-checkbox>
-        <div  :style="{'text-decoration': task.status ? 'line-through': '' }"> {{task.name}}</div> 
-        <small class="text-muted"><b-icon icon="clock"></b-icon> {{task.date}}</small>
+      <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+        v-for="(task,index) in tasks"
+        :key="index"
+      >
+        <div class="col-5">
+          <b-form-checkbox @change="task.status = !task.status" :checked="task.status">{{task.status ? 'Done':'Todo'}}</b-form-checkbox>
+          <div :style="{'text-decoration': task.status ? 'line-through': ''}">{{task.name}}</div>
+          <strong class="text-info">
+            <b-icon icon="clock"></b-icon>
+            {{task.date}} AT {{task.time}}
+          </strong>
         </div>
-        <button class="btn btn-danger btn-sm" @click="deleteTask(index)"><b-icon icon="trash"></b-icon></button>
+        <button class="btn btn-danger btn-sm" @click="deleteTask(index)">
+          <b-icon icon="trash"></b-icon>
+        </button>
       </li>
     </ul>
   </div>
@@ -19,10 +26,10 @@ export default {
   name: "List",
   props: ["tasks"],
   methods: {
-     deleteTask : function(index){
-       this.$delete(this.tasks, index);
-     },
-  },
+    deleteTask: function(index) {
+      this.$delete(this.tasks, index);
+    },
+  }
 };
 </script>
  
